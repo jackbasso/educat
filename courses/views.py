b1c9e134d6feaae44cf61921d57211a8d9ae703f
jackbasso <jackbasso.r@gmail.com>
2022-11-13 23:12:08 -0500

    student detail view

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Formsets imports
@@ -9,9 +10,11 @@ from .forms import ModuleFormSet
 # for view that handles creating or updating the objects of any content model
 from django.forms.models import modelform_factory
 from django.apps import apps
-from .models import Course, Module, Content
+from .models import Course, Module, Content, Subject
 #django-braces is a third-party module that contains a collection of generic mixins for Django
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
+from django.db.models import Count
 
 class OwnerMixin:
     def get_queryset(self):
@@ -153,3 +156,20 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
         for id, order in self.request_json.items():
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+    
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,'subject': subject,'courses': courses})
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
