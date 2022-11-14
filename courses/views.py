2b1e775b61724bc7326a72bb1e6bad6d8e8eba96
jackbasso <jackbasso.r@gmail.com>
2022-11-13 22:11:06 -0500

    drag and drop

--- courses/views.py
+++ courses/views.py
@@ -2,8 +2,16 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-
-from .models import Course
+# Formsets imports
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
+# for view that handles creating or updating the objects of any content model
+from django.forms.models import modelform_factory
+from django.apps import apps
+from .models import Course, Module, Content
+#django-braces is a third-party module that contains a collection of generic mixins for Django
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 class OwnerMixin:
     def get_queryset(self):
@@ -37,4 +45,111 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
 
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
 
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+        id=pk,
+        owner=request.user)
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course, 'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,'formset': formset})
+
+'''
+Class that allow you to create and update different models contents
+'''
+class ContentCreateUpdateView(TemplateResponseMixin, View):
+    module = None
+    model = None
+    obj = None
+    template_name = 'courses/manage/content/form.html'
+
+    def get_model(self, model_name):
+        if model_name in ['text', 'video', 'image', 'file']:
+            return apps.get_model(app_label='courses',model_name=model_name)
+        return None
+
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(model, exclude=['owner',
+            'order',
+            'created',
+            'updated'])
+        return Form(*args, **kwargs)
+    
+    def dispatch(self, request, module_id, model_name, id=None):
+        self.module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+        if id:
+            self.obj = get_object_or_404(self.model, id=id, owner=request.user)
+        return super().dispatch(request, module_id, model_name, id)
+
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return self.render_to_response({'form': form,'object': self.obj})
+    
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model,
+            instance=self.obj,
+            data=request.POST,
+            files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+        if not id:
+        # new content
+            Content.objects.create(module=self.module,item=obj)
+            return redirect('module_content_list', self.module.id)
+        return self.render_to_response({'form': form,'object': self.obj})
+
+class ContentDeleteView(View):
+    def post(self, request, id):
+        content = get_object_or_404(Content,
+            id=id,
+            module__course__owner=request.user)
+        module = content.module
+        content.item.delete()
+        content.delete()
+        return redirect('module_content_list', module.id)
+
+class ModuleContentListView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/content_list.html'
+    
+    def get(self, request, module_id):
+        module = get_object_or_404(Module, id=module_id, course__owner=request.user)
+        return self.render_to_response({'module': module})
+
+
+'''
+django-braces at https://django-braces.readthedocs.io/
+CsrfExemptMixin: Used to avoid checking the cross-site request forgery (CSRF) token in the
+POST requests. You need this to perform AJAX POST requests without the need to pass a csrf_
+token.
+JsonRequestResponseMixin: Parses the request data as JSON and also serializes the response
+as JSON and returns an HTTP response with the application/json content type.
+'''
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Module.objects.filter(id=id,course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+# Module content reorder
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
